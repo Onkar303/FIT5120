11e1d858b408670957bf37cea55bbed92dea2dc2
Onkar Kalpavriksha <okal0003@student.monash.edu>
2021-03-21 17:17:53 +1100

    Add project files.

---
+++ FIT5120_Project_Refugees/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5120_Project_Refugees.MvcApplication" Language="C#" %>
---
+++ FIT5120_Project_Refugees/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5120_Project_Refugees.Startup))]
+namespace FIT5120_Project_Refugees
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
