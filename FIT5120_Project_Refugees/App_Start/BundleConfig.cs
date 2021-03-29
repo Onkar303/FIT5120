a0ab06f4a15fc7b69391dbeabad3e0d7c9c471ef
Onkar Kalpavriksha <okal0003@student.monash.edu>
2021-03-29 19:28:16 +1100

    Added different sections Made the website more consistent change the cover image added change in language button added d3 with analytics charts

--- FIT5120_Project_Refugees/App_Start/BundleConfig.cs
+++ FIT5120_Project_Refugees/App_Start/BundleConfig.cs
@@ -14,6 +14,8 @@ namespace FIT5120_Project_Refugees
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/d3").Include("~/Scripts/d3/d3.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
--- FIT5120_Project_Refugees/Scripts/Index.js
+++ FIT5120_Project_Refugees/Scripts/Index.js
@@ -1,37 +1,74 @@
 ﻿
+var youtubeVideo = document.getElementById("youtubeVideo");
+youtubeVideo.setAttribute("height", screen.height/ 1.8);
+
 
 var dataset = [20, 50, 70, 45, 23];
 
-var width = 500;
-var height = 300;
+var width = screen.width/2;
+var height = screen.height/2;
 var barPadding = 5;
-var barWidth = width / dataset.length;
+var barWidth = width / dataset.length - 20;
 
 
-var svg = d3.select('barchart')
-    .attr("width", width)
-    .attr("height", height);
+    var svg = d3.select('svg')
+        .attr("width", width)
+        .attr("height", height);
 
 
-    
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset)])
+        .range([0, height]);
+
+    var xScale = d3.scaleLinear()
+        .domain([0, 5])
+        .range([0, width]);
+
+    var x_axis = d3.axisBottom().scale(xScale);
+    var y_axis = d3.axisLeft().scale(yScale);
+
+    svg.append("g")
+        .attr("transform", "translate(50,10)")
+        .call(y_axis)
+
+    var xAxisTranslate = height - 20;
+
+    svg.append("g")
+        .attr("transform", "translate(50," + xAxisTranslate + ")")
+        .call(x_axis);
+
+    var barChart = svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("y", function (d) {
+            return height - yScale(d)
+        })
+        .attr("height", function (d) {
+            return yScale(d) - 20;
+        })
+        .attr("width", barWidth - barPadding)
+        .attr("transform", function (d, i) {
+
+            var translate = [barWidth * i + 51, 0];
+            return "translate(" + translate + ")";
+        }); 
 
 
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function (d) {
-        return height - d
-    })
-    .attr("height", function (d) {
-        return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function (d, i) {
 
-        var translate = [barWidth * i, 0];
-        return "translate(" + translate + ")";
 
-    }); 
+//var text = svg.selectAll("text")
+//    .data(dataset)
+//    .enter()
+//    .append("text")
+//    .text(function (d) {
+//        return d;
+//    })
+//    .attr("y", function (d, i) {
+//        return height - d - 2;
+//    })
+//    .attr("x", function (d, i) {
+//        return barWidth * i;
+//    })
 
 
