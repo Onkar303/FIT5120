e581b239be05e05a81bd52a8603054cd56cde705
Onkar Kalpavriksha <okal0003@student.monash.edu>
2021-04-01 13:46:27 +1100

    Added interactable pie chart Made some minor changes

--- FIT5120_Project_Refugees/Scripts/Index.js
+++ FIT5120_Project_Refugees/Scripts/Index.js
@@ -63,4 +63,48 @@ englishTranslate.addEventListener("click", function () {
 
 
 
+let ctx = document.getElementById('myChart').getContext('2d');
+let labels = ['Iraq 🍕', 'Democratic Republic of Congo 🌮', 'Syria 🌭', 'Myanmar 🍣', 'Afghanistan', 'Eritrea', 'Ethiopia'];
+let colorHex = ['#FB3640', '#EFCA08', '#43AA8B', '#253D5B'];
+
+let myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [30, 10, 40, 20],
+            backgroundColor: colorHex
+        }],
+        labels: labels
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'bottom'
+        },
+        plugins: {
+            datalabels: {
+                color: '#fff',
+                anchor: 'end',
+                align: 'start',
+                offset: -10,
+                borderWidth: 2,
+                borderColor: '#fff',
+                borderRadius: 25,
+                backgroundColor: (context) => {
+                    return context.dataset.backgroundColor;
+                },
+                font: {
+                    weight: 'bold',
+                    size: '10'
+                },
+                formatter: (value) => {
+                    return value + ' %';
+                }
+            }
+        }
+    }
+})
+
+
+
 
