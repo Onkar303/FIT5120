6dd98147535a2da3c53e09308e2bdeabaf98ecc2
Onkar Kalpavriksha <okal0003@student.monash.edu>
2021-04-17 00:28:35 +1000

    Added a map for analytics page Removed redundant code aligned with the prototype

--- FIT5120_Project_Refugees/Scripts/analyticsScript.js
+++ FIT5120_Project_Refugees/Scripts/analyticsScript.js
@@ -1,4 +1,192 @@
-﻿let ctx = document.getElementById('pieChart').getContext('2d');
+﻿
+
+am4core.ready(function () {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    // Create map instance
+    var chart = am4core.create("chartdiv", am4maps.MapChart);
+    var title = chart.titles.create();
+    title.text = "Top 5 Countries";
+    title.fontSize = 30;
+    title.fontWeight = "bold";
+    title.color = "#D9C1A4";
+    chart.geodata = am4geodata_worldLow;
+    chart.paddingTop = 20;
+    chart.paddingBottom = 20;
+    chart.chartContainer.wheelable = false;
+    chart.seriesContainer.draggable = false;
+    chart.seriesContainer.resizable = false;
+    chart.maxZoomLevel = 1;
+    chart.projection = new am4maps.projections.Miller();
+
+    // Create map polygon series
+    var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.useGeodata = true;
+    polygonSeries.name = "Others";
+    polygonSeries.exclude = ["AQ"];
+
+
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    // polygonTemplate.fill = am4core.color("#74B266");
+    // Add heat rule
+
+    var hs = polygonTemplate.states.create("hover");
+
+    var series1 = chart.series.push(new am4maps.MapPolygonSeries());
+
+    series1.name = "Iraq";
+    series1.useGeodata = true;
+    series1.include = ["IQ"];
+    series1.mapPolygons.template.tooltipText = "{name}";
+    series1.mapPolygons.template.fill = am4core.color("#CD1725");
+    series1.fill = am4core.color("#CD1725");
+
+    var series2 = chart.series.push(new am4maps.MapPolygonSeries());
+    series2.name = "Democratic Republic of Congo";
+    series2.useGeodata = true;
+    series2.include = ["CD"];
+    series2.mapPolygons.template.tooltipText = "{name}";
+    series2.mapPolygons.template.fill = am4core.color("#2C88FE");
+    series2.fill = am4core.color("#2C88FE");
+
+
+    var series3 = chart.series.push(new am4maps.MapPolygonSeries());
+    series3.name = "Syria";
+    series3.useGeodata = true;
+    series3.include = ["SY"];
+    series3.mapPolygons.template.tooltipText = "{name}";
+    series3.mapPolygons.template.fill = am4core.color("#010101");
+    series3.fill = am4core.color("#010101");
+
+
+    var series4 = chart.series.push(new am4maps.MapPolygonSeries());
+    series4.name = "Myanmar";
+    series4.useGeodata = true;
+    series4.include = ["MM"];
+    series4.mapPolygons.template.tooltipText = "{name}";
+    series4.mapPolygons.template.fill = am4core.color("#259159");
+    series4.fill = am4core.color("#259159");
+
+
+    var series5 = chart.series.push(new am4maps.MapPolygonSeries());
+    series5.name = "Afganistan";
+    series5.useGeodata = true;
+    series5.include = ["AF"];
+    series5.mapPolygons.template.tooltipText = "{name}";
+    series5.mapPolygons.template.fill = am4core.color("#D99694");
+    series5.fill = am4core.color("#D99694");
+
+
+
+    //adding data
+    //polygonSeries.data = [{
+    //        "id": "IQ",
+    //        "name": "Iraq",
+    //        "value": 100,
+    //        "fill": am4core.color("#F05C5C")
+    //}, {
+    //        "id": "CG",
+    //        "name": "Democratic Reublic of Congo",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "IR",
+    //        "name": "Iran",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "SY",
+    //        "name": "Syria",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "MM",
+    //        "name": "Myanmar",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "AF",
+    //        "name": "Afganistan",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "ER",
+    //        "name": "Eritrea",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "ET",
+    //        "name": "Ethiopia",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "ER",
+    //        "name": "Eritrea",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    },
+    //    {
+    //        "id": "CF",
+    //        "name": "Central African Republic",
+    //        "value": 50,
+    //        "fill": am4core.color("#5C5CFF")
+    //    }
+        
+
+    //];
+
+    polygonTemplate.propertyFields.fill = "fill";
+
+    chart.legend = new am4maps.Legend();
+    chart.legend.position = "right";
+    chart.legend.align = "right";
+
+
+
+    //chart.legend = new am4maps.Legend();
+    //chart.legend.position = "right";
+    //chart.legend.align = "right";
+
+    //var container = am4core.create("container", am4core.Container);
+    //container.width = am4core.percent(100);
+    //container.height = am4core.percent(100);
+
+    // Series for United States map
+    // var usaSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    // usaSeries.geodata = am4geodata_usaLow;
+
+    // var usPolygonTemplate = usaSeries.mapPolygons.template;
+    // usPolygonTemplate.tooltipText = "{name}";
+    // usPolygonTemplate.fill = chart.colors.getIndex(1);
+    // usPolygonTemplate.nonScalingStroke = true;
+
+    // // Hover state
+    // var hs = usPolygonTemplate .states.create("hover");
+    // hs.properties.fill = am4core.color("#367B25");
+
+}); // end am4core.ready()
+
+
+
+
+
+
+
+
+
+
+/*let ctx = document.getElementById('pieChart').getContext('2d');*/
 let labels = ['Iraq ', 'Democratic Republic of Congo ', 'Syria ', 'Myanmar', 'Afghanistan', 'Eritrea', 'Ethiopia', 'Central African Republic', 'Iran', 'Stateless', 'Others'];
 let colorHex = ['#FB3640', '#EFCA08', '#43AA8B', '#253D5B', '#0A2244', '#E6E6E6', '#F3F3F3', '#FF0000', '#553B0A', '#1D550B', '#2B1555'];
 
@@ -17,181 +205,181 @@ var a2018_19 = document.getElementById('2018_19');
 var a2019_20 = document.getElementById('2019_20');
 
 
-a2015_16.addEventListener("click", function () {
-    updateData(no2015_16);
-})
+//a2015_16.addEventListener("click", function () {
+//    updateData(no2015_16);
+//})
 
-a2016_17.addEventListener("click", function () {
-    updateData(no2016_17);
-})
+//a2016_17.addEventListener("click", function () {
+//    updateData(no2016_17);
+//})
 
-a2017_18.addEventListener("click", function () {
-    updateData(no2017_18);
-})
+//a2017_18.addEventListener("click", function () {
+//    updateData(no2017_18);
+//})
 
-a2018_19.addEventListener("click", function () {
-    updateData(no2018_19);
-})
+//a2018_19.addEventListener("click", function () {
+//    updateData(no2018_19);
+//})
 
-a2019_20.addEventListener("click", function () {
-    updateData(no2019_20);
-})
+//a2019_20.addEventListener("click", function () {
+//    updateData(no2019_20);
+//})
 
-function updateData(selectedYear) {
-    pieChart.data.datasets[0].data = selectedYear;
-    pieChart.update();
-}
+//function updateData(selectedYear) {
+//    pieChart.data.datasets[0].data = selectedYear;
+//    pieChart.update();
+//}
 
-let pieChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: no2019_20,
-                backgroundColor: colorHex
-            }],
-            labels: labels
-        },
-        options: {
-            responsive:true,
-            title: {
-                display: true,
-                text: 'Total Refugees'
-            },
-            legend: {
-                position: 'bottom'
-            },
-            plugins: {
-                datalabels: {
-                    color: '#fff',
-                    anchor: 'end',
-                    align: 'start',
-                    offset: -10,
-                    borderWidth: 2,
-                    borderColor: '#fff',
-                    borderRadius: 25,
-                    backgroundColor: (context) => {
-                        return context.dataset.backgroundColor;
-                    },
-                    font: {
-                        weight: 'bold',
-                        size: '10'
-                    },
-                    formatter: (value) => {
-                        return value + ' people';
-                    }
-                }
-            }
-        }
-  })
-
-
-
-
-var male2015_16 = [2098,322,2158,978,842,166,167,0,172,0,821]; 
-var female2015_16 = [2260, 335, 2103, 973, 872, 127, 170, 0, 165, 0, 823];
-
-var male2016_17 = [3629,157,3122,896,943,166,167,0,62,0,843];
-var female2016_17 = [3859, 179,3139,851,1015,157,156,0,77,0,858];
-
-var male2017_18 = [2301,648,1603,1025,474,236,239,5,103,0,623];
-var female2017_18 = [2329,671,1624,1018,656,258,242,5,115,0,618];
-
-var male2018_19 = [3514,1048,935,1011,572,264,305,36,187,0,575];
-var female2018_19 = [3581,1066,901,984,751,291,330,36,180,0,545];
-
-var male2019_20 = [2856,568,475,367,275,166,168,161,146,86,281];
-var female2019_20 = [2929,597,472,429,344,268,192,172,171,0.280];
+//let pieChart = new Chart(ctx, {
+//        type: 'pie',
+//        data: {
+//            datasets: [{
+//                data: no2019_20,
+//                backgroundColor: colorHex
+//            }],
+//            labels: labels
+//        },
+//        options: {
+//            responsive:true,
+//            title: {
+//                display: true,
+//                text: 'Total Refugees'
+//            },
+//            legend: {
+//                position: 'bottom'
+//            },
+//            plugins: {
+//                datalabels: {
+//                    color: '#fff',
+//                    anchor: 'end',
+//                    align: 'start',
+//                    offset: -10,
+//                    borderWidth: 2,
+//                    borderColor: '#fff',
+//                    borderRadius: 25,
+//                    backgroundColor: (context) => {
+//                        return context.dataset.backgroundColor;
+//                    },
+//                    font: {
+//                        weight: 'bold',
+//                        size: '10'
+//                    },
+//                    formatter: (value) => {
+//                        return value + ' people';
+//                    }
+//                }
+//            }
+//        }
+//  })
 
 
 
-var mf2015_16 = document.getElementById('mf2015_16');
-var mf2016_17 = document.getElementById('mf2016_17');
-var mf2017_18 = document.getElementById('mf2017_18');
-var mf2018_19 = document.getElementById('mf2018_19');
-var mf2019_20 = document.getElementById('mf2019_20');
+
+//var male2015_16 = [2098,322,2158,978,842,166,167,0,172,0,821]; 
+//var female2015_16 = [2260, 335, 2103, 973, 872, 127, 170, 0, 165, 0, 823];
+
+//var male2016_17 = [3629,157,3122,896,943,166,167,0,62,0,843];
+//var female2016_17 = [3859, 179,3139,851,1015,157,156,0,77,0,858];
+
+//var male2017_18 = [2301,648,1603,1025,474,236,239,5,103,0,623];
+//var female2017_18 = [2329,671,1624,1018,656,258,242,5,115,0,618];
+
+//var male2018_19 = [3514,1048,935,1011,572,264,305,36,187,0,575];
+//var female2018_19 = [3581,1066,901,984,751,291,330,36,180,0,545];
+
+//var male2019_20 = [2856,568,475,367,275,166,168,161,146,86,281];
+//var female2019_20 = [2929,597,472,429,344,268,192,172,171,0.280];
 
 
-mf2015_16.addEventListener("click", function () {
-    updateBarChart(male2015_16,female2015_16);
-})
 
-mf2016_17.addEventListener("click", function () {
-    updateBarChart(male2016_17, female2016_17);
-})
-
-mf2017_18.addEventListener("click", function () {
-    updateBarChart(male2017_18, female2017_18);
-})
-
-mf2018_19.addEventListener("click", function () {
-    updateBarChart(male2018_19, female2018_19);
-})
-
-mf2019_20.addEventListener("click", function () {
-    updateBarChart(male2019_20, female2019_20);
-});
+//var mf2015_16 = document.getElementById('mf2015_16');
+//var mf2016_17 = document.getElementById('mf2016_17');
+//var mf2017_18 = document.getElementById('mf2017_18');
+//var mf2018_19 = document.getElementById('mf2018_19');
+//var mf2019_20 = document.getElementById('mf2019_20');
 
 
-function updateBarChart(selectedYearMale,selectedYearFemale) {
-    barChart.data.datasets[0].data = selectedYearMale;
-    barChart.data.datasets[1].data = selectedYearFemale;
-    barChart.update();
-}
+//mf2015_16.addEventListener("click", function () {
+//    updateBarChart(male2015_16,female2015_16);
+//})
+
+//mf2016_17.addEventListener("click", function () {
+//    updateBarChart(male2016_17, female2016_17);
+//})
+
+//mf2017_18.addEventListener("click", function () {
+//    updateBarChart(male2017_18, female2017_18);
+//})
+
+//mf2018_19.addEventListener("click", function () {
+//    updateBarChart(male2018_19, female2018_19);
+//})
+
+//mf2019_20.addEventListener("click", function () {
+//    updateBarChart(male2019_20, female2019_20);
+//});
 
 
-let barctx = document.getElementById('barChart').getContext('2d');
+//function updateBarChart(selectedYearMale,selectedYearFemale) {
+//    barChart.data.datasets[0].data = selectedYearMale;
+//    barChart.data.datasets[1].data = selectedYearFemale;
+//    barChart.update();
+//}
 
-let barChart = new Chart(barctx, {
-    type: 'bar',
-    data: {
-        datasets: [{
-            label:'male',
-            data: male2019_20,
-            backgroundColor:'#253D5B',
+
+//let barctx = document.getElementById('barChart').getContext('2d');
+
+//let barChart = new Chart(barctx, {
+//    type: 'bar',
+//    data: {
+//        datasets: [{
+//            label:'male',
+//            data: male2019_20,
+//            backgroundColor:'#253D5B',
             
-        }, {
-                label: 'female',
-                data: female2019_20,
-                backgroundColor: '#FB3640',
-          }],
-        labels: labels
-    },
-    options: {
-        responsive: true,
-        scaleShowValues: true,
-        scales: {
-            xAxes: [{
-                ticks: {
-                    autoSkip: false
-                }
-            }]
-        },
-        legend: {
-            position: 'bottom'
-        },
-        plugins: {
-            datalabels: {
-                color: '#fff',
-                anchor: 'end',
-                align: 'start',
-                offset: -10,
-                borderWidth: 2,
-                borderColor: '#fff',
-                borderRadius: 25,
-                backgroundColor: (context) => {
-                    return context.dataset.backgroundColor;
-                },
-                font: {
-                    weight: 'bold',
-                    size: '10'
-                },
-                formatter: (value) => {
-                    return value;
-                },
-            }
-        }
-    }
-})
+//        }, {
+//                label: 'female',
+//                data: female2019_20,
+//                backgroundColor: '#FB3640',
+//          }],
+//        labels: labels
+//    },
+//    options: {
+//        responsive: true,
+//        scaleShowValues: true,
+//        scales: {
+//            xAxes: [{
+//                ticks: {
+//                    autoSkip: false
+//                }
+//            }]
+//        },
+//        legend: {
+//            position: 'bottom'
+//        },
+//        plugins: {
+//            datalabels: {
+//                color: '#fff',
+//                anchor: 'end',
+//                align: 'start',
+//                offset: -10,
+//                borderWidth: 2,
+//                borderColor: '#fff',
+//                borderRadius: 25,
+//                backgroundColor: (context) => {
+//                    return context.dataset.backgroundColor;
+//                },
+//                font: {
+//                    weight: 'bold',
+//                    size: '10'
+//                },
+//                formatter: (value) => {
+//                    return value;
+//                },
+//            }
+//        }
+//    }
+//})
 
 
 var whoIsRefugee = document.getElementById('analytics');
